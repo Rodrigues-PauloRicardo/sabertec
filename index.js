8d5fc575c723d0cdaab215d33ee09f2f4064db15
Rodrigues-PauloRicardo <rodrigues.pauloricardo@gmail.com>
2023-11-08 14:18:08 -0300

    at

---
+++ index.js
@@ -0,0 +1,48 @@
+function RemoverCorFundoAtencao() {
+  var agua1 = document.querySelector("#agua2");
+  agua1.style.backgroundColor = "transparent"; // ou rgba(0, 0, 0, 0) para cor completamente transparente  
+}
+
+
+function NivelPoluicao() {  
+  var mensagem = "ATENÇÃO ALTO NÍVEL DE POLUIÇÃO ARROIO 001!";
+  var msgElement = document.querySelector(".msg2");
+  msgElement.textContent = mensagem;  
+
+  // Agende a restauração do texto após 5 segundos
+  setTimeout(function() {
+    msgElement.textContent = "ÓRGÃO PÚBLICO"; // Restaura o texto ao estado original (vazio)
+}, 5000); // 5000 milissegundos = 5 segundos 
+}
+
+function NivelAlerta() {
+  var agua2 = document.querySelector("#agua1");
+  agua2.style.backgroundColor = "transparent"; // ou rgba(0, 0, 0, 0) para cor completamente transparente
+  var mensagem = "ATENÇÃO RISCO DE INUNDAÇÃO!";
+  var msgElement = document.querySelector(".msg");
+  msgElement.textContent = mensagem;  
+  var alertaAudio = document.getElementById("alertaAudio");
+  alertaAudio.play();
+}
+
+function MudarColeta(id) {
+  var imagem = document.getElementById('imagemColeta');
+  imagem.style.display = 'block';
+}
+
+function LixoVisivel(id) {
+  var imagem = document.getElementById('lixo');
+  imagem.style.display = 'block';
+  var mensagem = "COLETOR 25ST CHEIO! ENVIAR CAMINHÃO PARA COLETA";
+  var msgElement = document.querySelector(".msg2");
+  msgElement.textContent = mensagem;
+}
+
+function LixoInvisivel(id) {
+  var imagem = document.getElementById('lixo');
+  imagem.style.display = 'none';
+  var msgElement = document.querySelector(".msg2");
+  msgElement.textContent = 'ÓRGÃO PÚBLICO';
+
+
+}
